cfc5036f2d311548bff675646c3136f561c641a5
Ethan Huang <yehuang2020@gmail.com>
2024-01-08 15:56:39 -0800

    added setColor and folder image

---
+++ components/setColor.js
@@ -0,0 +1,34 @@
+import styles from '../src/app/page.module.css';
+import { Component } from 'react';
+
+class SetColor extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className = {styles.chooseColor}>
+                <div className={styles.rowColor}>
+                {this.props.colors.map((color, idx) => {
+                        return (
+                            <button
+                                key = {idx}
+                                style={{
+                                backgroundColor: color,
+                                }} 
+                                className={styles.colorButton}
+                                >
+                                <p className={styles.sampleLetter}>a</p>
+                            </button>
+                        )
+                }               
+                )}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SetColor;
